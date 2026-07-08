c28a4f700920f26a45d3d7940cc034a79f777c55
Twoje Imię <[email removed]>
2026-07-08 16:10:02 +0100

    Create app.js

---
+++ pages/app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const script = document.createElement("script");
+  script.src = "../frontend/js/app.js";
+  document.body.appendChild(script);
+});
